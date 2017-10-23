86779d7de125aa5fc9a02101f15c3304915e28af
alvin niamatali and hansen hansen <alvin@>
2017-10-23 11:41:49 -0700

    test methods passing

---
+++ ToDoList.Tests/ModelTests/TaskTests.cs
@@ -0,0 +1,65 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ToDoList;
+using System.Collections.Generic;
+
+namespace ToDoList.Tests
+{
+
+  [TestClass]
+  public class TaskTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Task.DeleteAll();
+    }
+    public TaskTests()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=todo;";
+    }
+
+    [TestMethod]
+    public void GetAll_DatabaseEmptyAtFirst_0()
+    {
+      //Arrange, Act
+      int result = Task.GetAll().Count;
+      Console.WriteLine("result = " + result);
+
+      //Assert
+      Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public void Save_SavesToDatabase_TaskList()
+    {
+      //Arrange
+      Task testTask = new Task("Mow the lawn");
+
+      //Act
+      testTask.Save();
+      List<Task> result = Task.GetAll();
+      List<Task> testList = new List<Task>{testTask};
+
+      //Assert
+      CollectionAssert.AreEqual(testList, result);
+    }
+
+    [TestMethod]
+    public void Save_AssignsIdToObject_Id()
+    {
+    //Arrange
+    Task testTask = new Task("Mow the lawn");
+
+    //Act
+    testTask.Save();
+    Task savedTask = Task.GetAll()[0];
+
+    int result = savedTask.GetId();
+    int testId = testTask.GetId();
+
+    //Assert
+    Assert.AreEqual(testId, result);
+    }
+
+  }
+}
